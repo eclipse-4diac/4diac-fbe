08ecbfd76c1a17ddfa2e1b733e579b1c246ee933
Jörg Walter <joerg.walter@offis.de>
2024-06-26 20:14:51 +0200

    zlib update

--- dependencies/recipes/zlib/build.cmake
+++ dependencies/recipes/zlib/build.cmake
@@ -1,5 +1,5 @@
 #********************************************************************************
-# Copyright (c) 2018, 2024 OFFIS e.V.
+# Copyright (c) 2018, 2023 OFFIS e.V.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License 2.0 which is available at
@@ -10,7 +10,6 @@
 # Contributors:
 #    Jörg Walter - initial implementation
 # *******************************************************************************/
-#
 
 cmake_minimum_required(VERSION 3.13)
 PROJECT(zlib LANGUAGES C VERSION 1.2.11)
@@ -24,9 +23,11 @@ string(REGEX REPLACE " \\\${ZLIB_DLL_SRCS} " " " PATCHING "${PATCHING}")
 string(REGEX REPLACE "add_executable\\([^)]*\\)" "" PATCHING "${PATCHING}")
 string(REGEX REPLACE "add_test\\([^)]*\\)" "" PATCHING "${PATCHING}")
 string(REGEX REPLACE "target_link_libraries\\([^)]*\\)" "" PATCHING "${PATCHING}")
+string(REGEX REPLACE "target_include_directories\\([^)]*\\)" "" PATCHING "${PATCHING}")
 string(REGEX REPLACE "set_target_properties\\((example|minigzip)[^)]*\\)" "" PATCHING "${PATCHING}")
 file(WRITE ${CGET_CMAKE_ORIGINAL_SOURCE_FILE} "${PATCHING}")
 
+set(SKIP_INSTALL_ALL ON)
 set(SKIP_INSTALL_FILES ON)
 set(SKIP_INSTALL_LIBRARIES ON)
 include(${CGET_CMAKE_ORIGINAL_SOURCE_FILE})
@@ -34,6 +35,7 @@ set_target_properties(zlib PROPERTIES OUTPUT_NAME z)
 set_target_properties(zlibstatic PROPERTIES OUTPUT_NAME zx)
 
 install(TARGETS zlib EXPORT ${CMAKE_PROJECT_NAME} DESTINATION lib)
+install(FILES zlib.h ${CMAKE_CURRENT_BINARY_DIR}/zconf.h DESTINATION include)
 
 include(toolchain-utils)
 install_export_config()
